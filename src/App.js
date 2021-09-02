41d64b390a357177e4dccf72d2e6ec53aafd6728
= <wgagleb@gmail.com>
2021-09-02 23:29:34 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import {Products} from "./templates";
+import {Header} from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="main">
+        <Products />
+      </div>
+    </>
   );
 }
 
